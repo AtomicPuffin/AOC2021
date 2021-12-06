73e8c10df8db470e128381ef6e4f4e431d832bc5
AtomicPuffin <66074771+AtomicPuffin@users.noreply.github.com>
2021-12-06 15:15:39 +0100

    6

---
+++ AOC5piff.py
@@ -0,0 +1,102 @@
+import os
+import sys
+#import numpy as np
+os.chdir("/Users/andreas/Documents/GitHub/AOC2021/")
+input = open("input5.txt").readlines()
+#input = open("input5ex.txt").readlines()
+
+
+def create_sea():
+    temp = []
+    for i in range(1000):
+        temp.append([])
+        for y in range(1000):
+            temp[i].append(0)
+    return temp
+
+def read_lines(lines):
+    output = []
+    for i in lines:
+        output.append( [int(i.split()[0].split(",")[0]),int(i.split()[0].split(",")[1]),int(i.split()[-1].split(",")[0]),int(i.split()[-1].split(",")[1])])
+    return output
+
+def filter_lines_v(lines):
+    output = []
+    for i in lines:
+        if i[0] == i[2] or i[1] == i[3]:
+            output.append(i)
+    return output
+
+def filter_lines_d(lines):
+    output = []
+    for i in lines:
+        dx = abs(i[0]-i[2])
+        dy = abs(i[1]-i[3])
+        if dx == dy:
+            output.append(i)
+    return output
+
+def update_v(lines, seamap):
+    for i in lines:
+        x1 = i[0]
+        x2 = i[2]
+        y1 = i[1]
+        y2 = i[3]
+
+        if x1 == x2 and y1 == y2:
+            seamap[x1][y1] += 1
+
+        elif x1 == x2:
+            y = min(y1,y2)
+            d = abs(y2 - y1) +1
+
+            for t in range(d):
+                seamap[x1][y+t] += 1
+        else:
+            x = min(x1,x2)
+            d = abs(x2 - x1) +1
+
+            for t in range(d):
+                seamap[x+t][y1] += 1
+        
+def update_d(lines, seamap):
+    for i in lines:
+        dx = abs(i[0]-i[2])+1
+        if i[0]-i[1] == i[2]-i[3]:
+            x = max(i[0],i[2])
+            y = max(i[1],i[3])
+            for t in range(dx):
+                seamap[x-t][y-t] += 1   
+        else:
+            x = min(i[0],i[2])
+            y = max(i[1],i[3])
+            for t in range(dx):
+                seamap[x+t][y-t] += 1  
+
+         
+
+
+def count(seamap):
+    counter = 0
+    for x in seamap:
+        for y in x:
+            if y > 1:
+                counter += 1
+    return counter
+
+
+ventlines = read_lines(input)
+#print(ventlines)
+seamap = create_sea()
+ventlinesV = filter_lines_v(ventlines)
+ventlinesD = filter_lines_d(ventlines)
+update_v(ventlinesV,seamap)
+update_d(ventlinesD,seamap)
+
+
+
+#print(ventlinesV)
+#print(ventlinesD)
+#print(seamap)
+print(count(seamap))
+
---
+++ AOC6.py
@@ -0,0 +1,63 @@
+import os
+import sys
+#import numpy as np
+os.chdir("/Users/andreas/Documents/GitHub/AOC2021/")
+input = open("input6.txt").readlines()
+#input = open("input6ex.txt").readlines()
+
+
+def read_lines(lines):
+    output = []
+    temp = lines[0].split(",")
+    for i in temp:
+        output.append(int(i))
+    return output
+
+def initial_state(fishes, counter):
+    for x in range(len(fishes)):
+        counter[fishes[x]] += 1
+    return counter
+
+""" Part 1
+def update(fishes, days):
+    for i in range(days):
+        for x in range(len(fishes)):
+            if fishes[x] == 0:
+                fishes[x] = 6
+                fishes.append(8)
+            else:
+               fishes[x] -= 1
+"""
+def update(counter):
+    temp = counter[1]
+
+    counter[1] = counter[2]
+    counter[2] = counter[3]
+    counter[3] = counter[4]
+    counter[4] = counter[5]
+    counter[5] = counter[6]
+    counter[6] = counter[7]+counter[0]
+    counter[7] = counter[8]
+    counter[8] = counter[0]
+
+    counter[0] = temp
+
+
+counters = [0,0,0,0,0,0,0,0,0]
+
+school = read_lines(input)
+counters = initial_state(school, counters)
+
+for i in range (256):
+    update(counters)
+
+#print(counters)
+print(sum(counters))
+
+
+
+
+
+
+#print(len(school))
+
