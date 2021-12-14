0e0118fcb47e270fc5c6b89f252951faf72b5bda
AtomicPuffin <66074771+AtomicPuffin@users.noreply.github.com>
2021-12-14 10:12:53 +0100

    14 v2 edge bug fix

--- AOC2021_14.py
+++ AOC2021_14.py
@@ -6,17 +6,18 @@ import sys
 #import statistics
 os.chdir("/Users/andreas/Documents/GitHub/AOC2021/")
 input = open("input14.txt").readlines()
-#input = open("input14 copy.txt").readlines()
+input2 = open("input14 copy.txt").readlines()
 
 def readLines(lines):
     start = {}
     temp1 = lines.pop(0).rstrip()
+    edges = ''.join([temp1[0],temp1[-1]])
     temp2 = [temp1[i:i+2] for i in range(len(temp1[1:]))]
     for i in temp2:
         start[i] = start.get((i),0) + 1
     lines.pop(0)
     pins = {i[:2]: i.rstrip()[-1] for i in lines}
-    return start, pins
+    return start, pins, edges
         
 def insert(start, pins):
     end = {}
@@ -35,21 +36,25 @@ def spin(start, pins, spins):
         start = insert(start, pins)
     return start
 
-def calc1(end):
+def calc1(end, edges):
     result = {}
     for i in end:
         result[i[0]] = result.get((i[0]),0) + end[i]/2
         result[i[1]] = result.get((i[1]),0) + end[i]/2
-        print(result)
+    result[edges[0]] = result.get((edges[0]),0) + 0.5
+    result[edges[1]] = result.get((edges[1]),0) + 0.5
     most = max(result, key=result.get)
     least = min(result, key=result.get)
+
     return result[most] - result[least]
 
 
 
-s, p = readLines(input)
+s, p, e = readLines(input)
+s2, p2, e2 = readLines(input2)
 
 end = spin(s, p, 40)
+end2 = spin(s2, p2, 40)
 
-#print(end)
-print(calc1(end))
+print(calc1(end, e))
+print(calc1(end2, e2))
